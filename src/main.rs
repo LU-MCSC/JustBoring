ce16c75f0801dd0a7c71eaa34c0a164f5b1fab8a
Patrick Trottier <PTrottier@laurentian.ca>
2019-01-18 23:21:46 +0000

    Make interface more descriptive and reduce code

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,10 @@ use clap::App;
 fn main() { 
     // Load configurations
     let yaml = load_yaml!("cli.yml");
+
     // Get the input from user
     let matches = App::from_yaml(yaml).get_matches();
+
     // Parse the input from user
     let source = matches.value_of("source").unwrap();
     let target = matches.value_of("target").unwrap();
@@ -18,5 +20,5 @@ fn main() {
     // Display the commands
     let tunnel = format!("ssh -L 1234:mariadb.cs.laurentian.ca:22 {}@shell.cs.laurentian.ca", username.to_lowercase());
     let copy = format!("scp -P 1234 {}@127.0.0.1:{} {}", username, source, target); 
-    println!("Wants your password for shell:\n{}\nWants your mariadb password:\n{}", tunnel, copy);
+    println!("1. Open a shell and provide your password for shell after running:\n{}\n\n2. With the shell from the previous step idling, open a new shell and provide your mariadb password after running:\n{}", tunnel, copy);
 }
